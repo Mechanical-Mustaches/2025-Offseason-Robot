d07afe47fe2f70b8dbbd5601aa5cf7ea202cdb92
aebill14 <97410781+aebill14@users.noreply.github.com>
2025-09-09 17:05:27 -0500

    Updated CAN ID Values

--- src/main/java/frc/robot/subsystems/EndEffector.java
+++ src/main/java/frc/robot/subsystems/EndEffector.java
@@ -9,26 +9,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class EndEffector extends SubsystemBase {
 
-    // private SparkMax endEffectorMotor = new SparkMax(93, MotorType.kBrushless);
+    private SparkMax endEffectorMotor = new SparkMax(10, MotorType.kBrushless);
 
-    // public EndEffector(){
-    // SparkMaxConfig endEffectorConfig = new SparkMaxConfig();
+    public EndEffector(){
+    SparkMaxConfig endEffectorConfig = new SparkMaxConfig();
 
-    // endEffectorConfig
-    // .smartCurrentLimit(30)
-    // .idleMode(IdleMode.kBrake);
+    endEffectorConfig
+    .smartCurrentLimit(30)
+    .idleMode(IdleMode.kBrake);
 
-    // }
+    }
 
-    // public void grab(){
-    // endEffectorMotor.set(0.5);
-    // }
+    public void grab(){
+    endEffectorMotor.set(0.5);
+    }
 
-    // public void stop(){
-    // endEffectorMotor.set(0);
-    // }
+    public void stop(){
+    endEffectorMotor.set(0);
+    }
 
-    // public void release(){
-    // endEffectorMotor.set(-0.5);
-    // }
+    public void release(){
+    endEffectorMotor.set(-0.5);
+    }
 }
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -13,62 +13,58 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase{
 
-//     SwerveDriveSubsystem swerve;
+    SwerveDriveSubsystem swerve;
     
-//     private SparkMax intakeMotor = new SparkMax(99, MotorType.kBrushless);
-//     private SparkMax intakePivotLeft = new SparkMax(98, MotorType.kBrushless);
-//     private SparkMax intakePivotRight = new SparkMax(97, MotorType.kBrushless);
+    private SparkMax centeringMotor = new SparkMax(13, MotorType.kBrushless);
+    private SparkMax intakingMotor = new SparkMax(14, MotorType.kBrushless);
+    private SparkMax pivotMotor = new SparkMax(15, MotorType.kBrushless);
 
-//     public IntakeSubsystem(){
-//         SparkMaxConfig leftConfig = new SparkMaxConfig();
-//         SparkMaxConfig rightConfig = new SparkMaxConfig();
-//         ClosedLoopConfig pidConfig = new ClosedLoopConfig();
+    public IntakeSubsystem(){
+        SparkMaxConfig pivotConfig = new SparkMaxConfig();
+        ClosedLoopConfig pidConfig = new ClosedLoopConfig();
 
-//         pidConfig
-//             .pid(0.1, 0, 0)
-//             .feedbackSensor(FeedbackSensor.kPrimaryEncoder);
+        pidConfig
+            .pid(0.1, 0, 0)
+            .feedbackSensor(FeedbackSensor.kPrimaryEncoder);
         
-//         leftConfig
-//             .smartCurrentLimit(30)
-//             .idleMode(IdleMode.kBrake)
-//             .apply(pidConfig);
+        pivotConfig
+            .smartCurrentLimit(30)
+            .idleMode(IdleMode.kBrake)
+            .apply(pidConfig);
 
-//         rightConfig
-//             .apply(leftConfig)
-//             .follow(98, true);
-
-//             intakePivotLeft.configure(leftConfig, null, null);
-//             intakePivotRight.configure(rightConfig, null, null);
+            pivotMotor.configure(pivotConfig, null, null);
 
             
-//     }
-//     public double getEncoderValue(){
-//         return intakePivotLeft.getEncoder().getPosition();
-//     }
+    }
+    public double getEncoderValue(){
+        return pivotMotor.getEncoder().getPosition();
+    }
 
-//     public void pivotPickUp(){
-//         intakePivotLeft.getClosedLoopController().setReference(0, ControlType.kPosition);
-//     }
+    public void pivotPickUp(){
+        pivotMotor.getClosedLoopController().setReference(0, ControlType.kPosition);
+    }
 
-//     public void pivotDropOff(){
-//         intakePivotLeft.getClosedLoopController().setReference(1, ControlType.kPosition);
-//     }
+    public void pivotDropOff(){
+        pivotMotor.getClosedLoopController().setReference(1, ControlType.kPosition);
+    }
 
-//     public void intakeSpin(){
-//         intakeMotor.set(0.5);
-//     }
+    public void intakeSpin(){
+        intakingMotor.set(0.5);
+        centeringMotor.set(0.5);
+    }
 
-//     public void intakeStop(){
-//         intakeMotor.set(0);
-//     }
+    public void intakeStop(){
+        intakingMotor.set(0);
+        centeringMotor.set(0);
+    }
 
-//     public boolean isCoralDetected(){
-//         if ((swerve.leftDistanceSensor.getRange() + swerve.rightDistanceSensor.getRange())/2 < 5){
-//             return true;
-//         } else{
-//             return false;
-//         }
-//     }
+    public boolean isCoralDetected(){
+        if ((swerve.leftDistanceSensor.getRange() + swerve.rightDistanceSensor.getRange())/2 < 5){
+            return true;
+        } else{
+            return false;
+        }
+    }
 }
 
 
